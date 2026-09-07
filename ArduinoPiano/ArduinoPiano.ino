4d49e10b148a09fb89aeff79d691abb43a7d8450
Gab071 <kaminskigabriel07@gmail.com>
2026-09-07 17:12:40 +0300

    finished project

--- ArduinoPiano/ArduinoPiano.ino
+++ ArduinoPiano/ArduinoPiano.ino
@@ -10,29 +10,32 @@
 #define SwYellow 5 
 #define SwGreen 4
 
+const int LedAmount = 4;
 const int Tones[LedAmount] = { NOTE_C3, NOTE_E3, NOTE_G3, NOTE_C4 };
 
-const int LedAmount = 4;
 const int Leds[LedAmount] = {LedBlue, LedRed, LedYellow, LedGreen};
 const int Buttons[LedAmount] = {SwBlue, SwRed, SwYellow, SwGreen};
 
-void setup() {
+void setup() 
+{
   pinMode(Buzzer, OUTPUT);
   pinMode(LedBlue, OUTPUT);
   pinMode(LedRed, OUTPUT);
   pinMode(LedYellow, OUTPUT);
   pinMode(LedGreen, OUTPUT);
 
-  pinMode(SwBlue, OUTPUT);
-  pinMode(SwRed, OUTPUT);
-  pinMode(SwYellow, OUTPUT);
-  pinMode(SwGreen, OUTPUT);
+  pinMode(SwBlue, INPUT_PULLUP);
+  pinMode(SwRed, INPUT_PULLUP);
+  pinMode(SwYellow, INPUT_PULLUP);
+  pinMode(SwGreen, INPUT_PULLUP);
 }
 
-void loop() {
-
+void loop()
+{
+  ReadStateOfTheButtons();
 }
 
+// Function that plays corresponding Tone
 void PlayTone(int index)
 {
   if(index >=0 && index <= 4)
@@ -44,6 +47,23 @@ void PlayTone(int index)
     digitalWrite(Leds[index], LOW);
     noTone(Buzzer);
     delay(50);
+  }
+}
+
+// Function checks if button is pressed
+// if yes - then it plays a tone and turns on appropriate LED 
+void ReadStateOfTheButtons()
+{
+  for(int sw=0; sw<LedAmount; sw++)
+  {
+    if(digitalRead(Buttons[sw]) == LOW)
+    {
+      PlayTone(sw);
+    }
+    else
+    {
+      continue;
+    }
   }
 }
 
