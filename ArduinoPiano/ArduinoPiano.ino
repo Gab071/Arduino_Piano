debe86dacc37700fb27e94cdd1c271898b56af22
Gab071 <kaminskigabriel07@gmail.com>
2026-09-08 13:23:09 +0300

    fixed functions prototype

--- ArduinoPiano/ArduinoPiano.ino
+++ ArduinoPiano/ArduinoPiano.ino
@@ -11,7 +11,7 @@
 #define SwGreen 4
 
 void ReadStateOfTheButtons();
-void PlayTone(int index)
+void PlayTone(int index);
 
 const int LedAmount = 4;
 const int Tones[LedAmount] = {NOTE_C3, NOTE_E3, NOTE_G3, NOTE_C4};
